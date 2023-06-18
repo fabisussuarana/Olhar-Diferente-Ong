9aa363d9221204ffb35424903c6edf09454bfd65
fabisussuarana <bianasussuaranadasilva17@gmail.com>
2023-06-18 11:15:13 -0500

    aaa

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,20 +16,14 @@ function initScrollSuave(){
     const linksInternos = document.querySelectorAll('.link-interno-js a[href^="#"]');
   
     function scrollToSection(event){
-    // Evita que ocorra o scroll padrão, sem o efeito suave
+    
       event.preventDefault();
 
-      // pegando o valor do atributo href dos links
       const href = event.currentTarget.getAttribute('href');
-      
-      // usando esse valor de href para selecionar as sections
       const section = document.querySelector(href);
       
-      // scroll
       section.scrollIntoView({
-        // scroll fica suave
         behavior: 'smooth',
-        // alinha no começo da section
         block: 'start', 
       })
     }
