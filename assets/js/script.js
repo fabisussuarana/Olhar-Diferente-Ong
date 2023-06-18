af83faf555e2e6a5c3c18b73927177f8604c745e
fabisussuarana <bianasussuaranadasilva17@gmail.com>
2023-06-18 11:14:00 -0500

    scroll suave

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,42 @@
-const btn = document.getElementById('btn-js');
-
-function toggleMenu(){
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
+function abreMenu(){
+    const btn = document.getElementById('btn-js');
+    
+    function toggleMenu(){
+        const nav = document.getElementById('nav');
+        nav.classList.toggle('active');
+    }
+    
+    btn.addEventListener('click', toggleMenu);
 }
 
-btn.addEventListener('click', toggleMenu);
+abreMenu();
+
+
+function initScrollSuave(){
+    const linksInternos = document.querySelectorAll('.link-interno-js a[href^="#"]');
+  
+    function scrollToSection(event){
+    // Evita que ocorra o scroll padrão, sem o efeito suave
+      event.preventDefault();
+
+      // pegando o valor do atributo href dos links
+      const href = event.currentTarget.getAttribute('href');
+      
+      // usando esse valor de href para selecionar as sections
+      const section = document.querySelector(href);
+      
+      // scroll
+      section.scrollIntoView({
+        // scroll fica suave
+        behavior: 'smooth',
+        // alinha no começo da section
+        block: 'start', 
+      })
+    }
+  
+    linksInternos.forEach((link) => {
+      link.addEventListener('click', scrollToSection);
+    });
+  }
+  
+  initScrollSuave();
