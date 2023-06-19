2f9ca3bd01aa94b749dfb3dfe699796132799c8b
fabisussuarana <bianasussuaranadasilva17@gmail.com>
2023-06-18 21:20:55 -0500

    aa

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,42 @@
-function abreMenu(){
+function abreMenu() {
     const btn = document.getElementById('btn-js');
-    
-    function toggleMenu(){
+
+    function toggleMenu() {
         const nav = document.getElementById('nav');
         nav.classList.toggle('active');
     }
-    
+
     btn.addEventListener('click', toggleMenu);
 }
 
 abreMenu();
 
 
-function initScrollSuave(){
+function initScrollSuave() {
     const linksInternos = document.querySelectorAll('.link-interno-js a[href^="#"]');
-  
-    function scrollToSection(event){
-    
-      event.preventDefault();
 
-      const href = event.currentTarget.getAttribute('href');
-      const section = document.querySelector(href);
-      
-      section.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start', 
-      })
+    function scrollToSection(event) {
+
+        event.preventDefault();
+
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        })
+
+        linksInternos.forEach((link) => {
+            link.classList.remove('active');
+        });
+
+        event.currentTarget.classList.add('active');
     }
-  
+
     linksInternos.forEach((link) => {
-      link.addEventListener('click', scrollToSection);
+        link.addEventListener('click', scrollToSection);
     });
-  }
-  
-  initScrollSuave();
+}
+
+initScrollSuave();
